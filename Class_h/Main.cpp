d88d05303f8a4d4bc7288dce9b784a3af03e06d1
tekater <120014627+tekater@users.noreply.github.com>
2023-05-10 21:12:41 +0900

    Update Main.cpp

--- Class_h/Main.cpp
+++ Class_h/Main.cpp
@@ -16,11 +16,19 @@ void main() {
     srand(time(NULL));
 
     Student s;
-  
+
     int ball[3]{ 12,10,5 };
     char name[20]{ "Tenno" };
 
     s.init(name, ball);
     s.out();
     cout << "Средний балл: " << s.Final() << "\n";
+    cout << "get: " << s.GetRank(1) << "\n";
+    s.SetRank(1, 3);
+
+    cout << "\n2:\n\n";
+
+    cout << "Get: " << s.getName() << "\n";
+    s.setName("Lotos");
+    s.out();
 }
